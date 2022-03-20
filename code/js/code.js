fd3259d902fa5f5203725ac0ea09b96dfd2d60db
DjRedstone <djredstone64@gmail.com>
2022-03-20 11:40:39 +0100

    UPDATE 20/03/2022

--- code/js/code.js
+++ code/js/code.js
@@ -1,7 +1,7 @@
 
 const timestamp = 1650751200;
 
-var time = timestamp * 1000;
+const time = timestamp * 1000;
 var date = new Date(time - Date.now());
 
 var j = 0;
@@ -12,7 +12,7 @@ var s = 0;
 function updateTime() {
     date = new Date(time - Date.now());
     
-    j = date.getDay() + date.getMonth() * 31;
+    j = Math.floor(date / (1000 * 60 * 60 * 24))
     h = date.getHours();
     m = date.getMinutes();
     s = date.getSeconds();
